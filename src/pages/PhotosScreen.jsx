ab7e84bd93258547c17749fb6635a1c13ce97743
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-10-27 21:35:26 -0500

    Demo de paginación terminado

--- src/pages/PhotosScreen.jsx
+++ src/pages/PhotosScreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useLocation } from 'react-router'
+import { useHistory, useLocation } from 'react-router'
 
 export const PhotosScreen = () => {
     
@@ -8,16 +8,33 @@ export const PhotosScreen = () => {
 
     const { search } = useLocation();
     
-    const params = new URLSearchParams(search);
-    const skip = params.get("skip") || 0;
-    const limit = params.get("limit") || 10;
+    const query = new URLSearchParams(search);
+
+    const skip = parseInt(query.get('skip')) || 0;
+    const limit = parseInt(query.get('limit')) || 10;
 
     console.log(`Skip: ${skip}, Limit: ${limit}`);
+
+    const history = useHistory();
+
+    const handleNext = () => {
+        query.set('skip', skip + 10);
+        
+        // history.push({
+        //     pathname: './home',
+        //     search:'?skip=100&limit=1000'
+        // });
+
+        history.push({
+            search:query.toString()
+        });
+    }
 
     return (
         <div>
             <h1>PhotosScreen</h1>
             <p><b>Params</b> Skip:{skip}, Limit:{limit}</p>
+            <button onClick={() => handleNext()}>Next Page</button>
         </div>
     )
 }
