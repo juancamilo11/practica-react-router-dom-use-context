bd77816043e7f95fd5f4823d8d23cc03042a8de2
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-10-28 01:38:14 -0500

    Se renombraron las rutas privadas

--- src/pages/DashboardScreen.jsx
+++ src/pages/DashboardScreen.jsx
@@ -5,7 +5,7 @@ export const PDashboardScreen = () => {
         <div>
             <h1>DashboardScreen (Private)</h1>
             <hr />
-            
+            <p>Esta vista es sólo accesible sólo mientras tienes iniciada la sesión</p>
         </div>
     )
 }
